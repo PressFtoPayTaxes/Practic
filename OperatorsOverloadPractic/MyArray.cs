39531aa62b401959787ba2994c6a85a84cabe075
PressFtoPayTaxes <46023537+PressFtoPayTaxes@users.noreply.github.com>
2019-03-26 18:21:51 +0600

    Init

---
+++ OperatorsOverloadPractic/MyArray.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsOverloadPractic
+{
+    // 2
+    class MyArray
+    {
+        private const int ARRAY_SIZE = 10;
+        private int[] array = new int[ARRAY_SIZE];
+
+        public static bool operator > (MyArray firstArray, MyArray secondArray)
+        {
+            int firstSum = 0;
+            int secondSum = 0;
+
+            foreach (int number in firstArray.array)
+            {
+                firstSum += number;
+            }
+            foreach (int number in secondArray.array)
+            {
+                secondSum += number;
+            }
+
+            if (firstSum > secondSum)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator <(MyArray firstArray, MyArray secondArray)
+        {
+            int firstSum = 0;
+            int secondSum = 0;
+
+            foreach (int number in firstArray.array)
+            {
+                firstSum += number;
+            }
+            foreach (int number in secondArray.array)
+            {
+                secondSum += number;
+            }
+
+            if (firstSum < secondSum)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ OperatorsOverloadPractic/SomeClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsOverloadPractic
+{
+    // 1
+    class SomeClass
+    {
+        public int Property { get; set; }
+        public int AnotherProperty { get; set; }
+
+        public static bool operator == (SomeClass operand1, SomeClass operand2)
+        {
+            if (operand1.Property == operand2.Property && operand1.AnotherProperty == operand2.AnotherProperty)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator != (SomeClass operand1, SomeClass operand2)
+        {
+            if (operand1.Property != operand2.Property || operand1.AnotherProperty != operand2.AnotherProperty)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
