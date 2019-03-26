48b6382e20a07fe1925197abe1cc316778484c99
PressFtoPayTaxes <46023537+PressFtoPayTaxes@users.noreply.github.com>
2019-03-26 18:19:44 +0600

    Init

---
+++ HarryPotterAPI/Charachter.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarryPotterAPI
+{
+    [Serializable]
+    public class Charachter
+    {
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public Genders Gender { get; set; }
+        public Houses? House { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public int? YearOfBirth { get; set; }
+        public string Ancestry { get; set; }
+        public string EyeColour { get; set; }
+        public string HairColour { get; set; }
+        public Wand Wand { get; set; }
+        public string Patronus { get; set; }
+        public bool HogwartsStudent { get; set; }
+        public bool HogwartsStaff { get; set; }
+        public string Actor { get; set; }
+        public bool Alive { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ HarryPotterAPI/Program.cs
@@ -0,0 +1,36 @@
+﻿using DotNetXmlHttpRequest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using Newtonsoft.Json.Converters;
+
+namespace HarryPotterAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Charachter[] charachters;
+
+            string jsonUrl = "http://hp-api.herokuapp.com/api/characters";
+
+            XMLHttpRequest jsonData = new XMLHttpRequest();
+            jsonData.Open(XMLHttpRequest.EnumMethod.Get, jsonUrl);
+            jsonData.Send();
+
+            using (StreamReader streamReader = new StreamReader(jsonData.ResponseStream))
+            {
+                string jsonFile = streamReader.ReadToEnd();
+                var dateTimeConverter = new IsoDateTimeConverter { DateTimeFormat = "dd-MM-yyyy" };
+                
+                charachters = JsonConvert.DeserializeObject<Charachter[]>(jsonFile, dateTimeConverter);
+            }
+                        
+            
+        }
+    }
+}
