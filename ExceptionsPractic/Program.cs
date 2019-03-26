d547f8a6af058ca06d8ee43c3fe6d1057ba832e0
PressFtoPayTaxes <46023537+PressFtoPayTaxes@users.noreply.github.com>
2019-03-26 18:09:36 +0600

    Init

---
+++ ExceptionsPractic/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsPractic
+{
+    class Program
+    {
+        public static void SummonedMethod()
+        {
+                throw new Exception();
+        }
+
+        public static void SummoningMethod()
+        {
+            try
+            {
+                SummonedMethod();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Произошла рекурсия...");
+                SummoningMethod();
+            }
+        }
+        static void Main(string[] args)
+        {
+            SummoningMethod();
+        }
+
+
+
+    }
+}
