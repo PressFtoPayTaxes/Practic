037c3f28c808f4d4f77a959836009b65baa745b4
PressFtoPayTaxes <46023537+PressFtoPayTaxes@users.noreply.github.com>
2019-03-27 15:58:44 +0600

    Init

---
+++ Statistics/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Statistic stat = new Statistic();
+
+            string text = "Вот дом, Который построил Джек. А это пшеница, Которая в темном  чулане хранится В доме, " +
+                "Который построил Джек. А это веселая птица-синица, Которая часто ворует пшеницу, Которая в темном " +
+                "чулане хранится В доме, Который построил Джек.";
+
+            Dictionary<string, int> words = stat.WordsCount(text);
+
+            int counter = 1;
+            Console.WriteLine("\tСлово:\t\tКол-во:");
+
+            int tabSpace = 8;
+            foreach(var word in words)
+            {
+                if (word.Key != "всего")
+                {
+                    if (word.Key.Length >= tabSpace)
+                        Console.WriteLine($"{counter}.\t{word.Key}\t{word.Value}");
+                    else
+                        Console.WriteLine($"{counter}.\t{word.Key}\t\t{word.Value}");
+                    counter++;
+                }
+            }
+
+            Console.WriteLine($"Всего слов: {words["всего"]} из них уникальных: {counter - 1}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Statistics/Statistic.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statistics
+{
+    public class Statistic
+    {
+        private Dictionary<string, int> _words;
+
+        public Dictionary<string, int> WordsCount(string text)
+        {
+            _words = new Dictionary<string, int>();
+            string[] words = text.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].Trim(',', '.', '-');
+            }
+
+            int spacesCount = 0;
+            foreach(string word in words)
+            {
+                if (_words.ContainsKey(word))
+                {
+                    _words[word]++;
+                }
+                else if (word != "")
+                {
+                    _words.Add(word, 1);
+                }
+                else
+                    spacesCount++;
+
+            }
+            _words.Add("всего", words.Length - spacesCount);
+            return _words;
+        }
+    }
+}
