b04ce763ccff266c2393d89a3f95a2145e5e2782
PressFtoPayTaxes <46023537+PressFtoPayTaxes@users.noreply.github.com>
2019-04-03 21:05:09 +0600

    Init

---
+++ InterfacesPractic2/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesPractic2
+{
+    public class Student : IStudent
+    {
+        public string FullName { get; set; }
+
+        public int[] Grades{ get; set; }
+
+        public string Name { get; set; }
+
+        public double GetAvgGrade()
+        {
+            int sum = 0;
+            foreach(var grade in Grades)
+            {
+                sum += grade;
+            }
+
+            return sum / Grades.Count();
+        }
+
+        public string GetFullName()
+        {
+            return FullName;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+    }
+}
