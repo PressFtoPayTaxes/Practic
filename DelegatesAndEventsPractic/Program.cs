ac5d2aa801ebb608b3235e4350748a9d6b622918
PressFtoPayTaxes <46023537+PressFtoPayTaxes@users.noreply.github.com>
2019-04-04 18:17:12 +0600

    Add files via upload

---
+++ DelegatesAndEventsPractic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEventsPractic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass test = new TestClass
+            {
+                TestProperty = "Ы"
+            };
+
+            test.PropertyChanged += (sender, element) => Console.WriteLine($"{sender} изменил свойство экземпляра класса {element}");
+
+            test.TestProperty = "Ыы";
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DelegatesAndEventsPractic/TestClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEventsPractic
+{
+    public delegate void PropertyEventHandler(object sender, TestClass e);
+    public class TestClass : IPropertyChanged
+    {
+        public event PropertyEventHandler PropertyChanged;
+        public string User { get; set; } = "Игорь";
+        private string testProperty;
+        public string TestProperty
+        {
+            get
+            {
+                return testProperty;
+            }
+            set
+            {
+                PropertyChanged?.Invoke(User, this);
+                testProperty = value;
+            }
+        }
+    }
+}
